1abab9512289d2e7cba5b09af45cbc2ece81f606
Alexander Davies <xanderdavies@Alexanders-MacBook-Pro.local>
2020-02-25 15:38:12 -0500

    DONE

--- color.mli
+++ color.mli
@@ -1,7 +1,9 @@
-type color
-type color_name
-val to_color : int -> int -> int -> color
-val red : color -> int
-val green : color -> int
-val blue : color -> int
-val color_named : color_name -> color
+
+    type color
+    type color_name
+    val to_color : int -> int -> int -> color
+    val red : color -> int
+    val green : color -> int
+    val blue : color -> int
+    val color_named : color_name -> color
+
